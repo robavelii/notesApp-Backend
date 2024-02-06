0fa0233f2775f0abb3c7717ddd236c3c0eb9773c
Robel Fekadu <56929107+robavelii@users.noreply.github.com>
2024-02-06 10:45:36 +0300

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,4 @@
-// const allowedOrigins = ['https://notes-app-front-end-dun.vercel.app'];
-const allowedOrigins = ['*'];
+const allowedOrigins = ['https://notes-app-front-end-dun.vercel.app'];
+// const allowedOrigins = ['*'];
 
 module.exports = allowedOrigins;
